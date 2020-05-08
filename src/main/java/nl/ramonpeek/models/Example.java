1033614e79aa9c1f7532a451c9c594e4002d59c9
Ramon <r.peek@student.fontys.nl>
2020-05-08 10:33:47 +0200

    Initial project commit

---
+++ src/main/java/nl/ramonpeek/models/Example.java
@@ -0,0 +1,31 @@
+package nl.ramonpeek.models;
+
+public class Example {
+
+    private int id;
+    private String name;
+
+    public Example() {}
+
+    public Example(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
